66a26b7fe5758f69ec3f411bb09d3d8832b69b8f
Sean Freiburg <frycicle@gmail.com>
2014-10-25 21:26:49 -0700

    hey

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to user_post_path(@post.user_id, @post), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
